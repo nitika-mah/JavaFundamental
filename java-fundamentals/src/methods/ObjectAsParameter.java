a2a358e892a8d0d2c8882371bd9ad98cf72f2de7
nitika-mah <nitikamahiya07@gmail.com>
2025-04-21 22:21:48 +0530

    param passing

--- java-fundamentals/src/methods/ObjectAsParameter.java
+++ java-fundamentals/src/methods/ObjectAsParameter.java
@@ -8,8 +8,25 @@ public class ObjectAsParameter {
 
   public static void main(String[] args) {
     int A[] = {2, 3, 4, 5, 6}; 
-    update(A); 
-    System.out.println(A[0]);
+    update(A);
+    change(A, 2, 23);
+    for(int x: A) {
+      System.out.println(x);
+    }
+
+    // primitive type : 
+    int x = 10;
+    change2(x, 12);
+    System.out.println(x);
+
+  }
+
+  static void change(int A[], int index, int value) {
+    A[index] = value;
+  }
+
+  static void change2(int x, int value) {
+    x = value;
   }
   
 }
